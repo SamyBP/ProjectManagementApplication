aa65f5c1eeacdd493726c467ae6e7ea813127c09
Beni Pintea <b.pintea04@gmail.com>
2024-11-19 00:38:54 +0200

    added pytest and tested obtain token view

--- server/authentication/tests.py
+++ server/authentication/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client() -> APIClient:
+    return APIClient()
+
+
+@pytest.fixture
+def user_info() -> dict:
+    user_info = {
+        "username": 'test',
+        "password": 'testPassword123'
+    }
+    User.objects.create_user(**user_info)
+    return user_info
+
+
+@pytest.mark.django_db
+def test_whenObtainTokenPairValidCredentials_returnsAccessRefreshAndOk(api_client: APIClient, user_info: dict):
+    url = reverse("Obtain access and refresh token")
+    response = api_client.post(url, data=user_info)
+
+    assert response.status_code == 200
+    assert "access" in response.data
+    assert "refresh" in response.data
+
+
+@pytest.mark.django_db
+def test_whenObtainTokenPairWrongCredentials_returnsUnauthorized(api_client: APIClient):
+    url = reverse("Obtain access and refresh token")
+    response = api_client.post(url, data={"username": "wrong", "password": "wrong"})
+
+    assert response.status_code == 401
+    assert "detail" in response.data
+
+
+@pytest.mark.django_db
+def test_whenObtainTokenPairInvalidBodyStructure_returnsBadRequest(api_client: APIClient):
+    url = reverse("Obtain access and refresh token")
+    no_body_response = api_client.post(url, data={})
+    unexpected_body_response = api_client.post(url, data={"wrong": "will result in 400"})
+
+    assert no_body_response.status_code == 400
+    assert unexpected_body_response.status_code == 400
